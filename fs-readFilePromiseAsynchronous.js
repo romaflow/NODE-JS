1515e788848a2ad4c04d211ce8c77f28f8fd2cd7
romaflow <ylor.romaflow5@gmail.com>
2026-06-04 17:48:52 -0400

    asyncrono

--- fs-readFilePromiseAsynchronous.js
+++ fs-readFilePromiseAsynchronous.js
@@ -1,3 +1,4 @@
+// ESTE SOLO EN LOS MODULOS NATIVOS QUE NO TIENEN PROMESAS
 const fs = require('node:fs')
 
 const { promisify } = require('node:util')
@@ -5,15 +6,17 @@ const readFilePromise = promisify(fs.readFile)
 
 console.log('Leyendo el primer archivo')
 
-fs.readFilePromise('./archivo.txt', 'utf-8')
+readFilePromise('./archivo.txt', 'utf-8')
     .then(text => {
-        console.log('--1-- Texto: ', text);
+        console.log('--1-- Texto: ', text)
     })
+    .catch(err => console.error('Error en archivo 1:', err));
 
 console.log('----Hacer cosas mientras lee el archivo----')
 
 console.log('Leyendo el segundo archivo')
-fs.readFilePromise('./archivo2.txt', 'utf-8')
+readFilePromise('./archivo2.txt', 'utf-8')
     .then(text => {
         console.log('--2-- Texto: ', text)
     })
+    .catch(err => console.error('Error en archivo 2:', err));   
