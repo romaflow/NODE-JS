b0c71164a5d9182dc0bf8d8bd4a71ecfc1109b79
romaflow <ylor.romaflow5@gmail.com>
2026-06-12 18:53:44 -0400

    ultima actualizacion

--- fs-readFileAsyncAwaitIIFE.js
+++ fs-readFileAsyncAwaitIIFE.js
@@ -1,7 +1,29 @@
 const { readFile } = require('node:fs/promises');
 
+async function init() {
+
+    console.log('Leyendo el primer archivo')
+
+    const text = await readFile('./archivo.txt', 'utf-8')
+    console.log('--1-- Texto: ', text)
+
+    console.log('----Hacer cosas mientras lee el archivo----')
+
+    console.log('Leyendo el segundo archivo')
+
+    const secondText = await readFile('./archivo2.txt', 'utf-8')
+    console.log('--2-- Texto: ', secondText)
+}
+
+init()
+
+// Otra forma de hacerlo es con una IIFE, es decir, una función que se ejecuta 
+// inmediatamente despues de ser definida. Esto es útil para evitar 
+// tener que definir una función y luego llamarla, 
+// y también para mantener el código más limpio y organizado. 
+
 // IIFE INMEDIATLY INVOQUED FUNCTION EXPRESSION
-(
+/*(
     async () => {
     console.log('Leyendo el primer archivo')
 
@@ -14,4 +36,4 @@ const { readFile } = require('node:fs/promises');
 
     const secondText = await readFile('./archivo2.txt', 'utf-8')
     console.log('--2-- Texto: ', secondText)
-})()    
+})()    */
---
+++ fs-readFileAsyncAwaitParallel.js
@@ -0,0 +1,23 @@
+import { readFile } from 'node:fs/promises'
+
+const [text, secondText] = Promise.all([
+    readFile('./archivo.txt', 'utf-8'),
+    readFile('./archivo2.txt', 'utf-8')
+]).then([text, secondText => {
+    console.log('--1-- Texto: ', text)
+    console.log('--2-- Texto: ', secondText)
+}])
+
+/*
+console.log('Leyendo el primer archivo')
+
+const text = await readFile('./archivo.txt', 'utf-8')
+console.log('--1-- Texto: ', text)
+
+console.log('----Hacer cosas mientras lee el archivo----')
+
+console.log('Leyendo el segundo archivo')
+
+const secondText = await readFile('./archivo2.txt', 'utf-8')
+console.log('--2-- Texto: ', secondText)
+*/
